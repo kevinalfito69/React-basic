a849b658a0addeef836c965db25fafd3498e03f4
kevinalfito69 <kevinalfito69@gmail.com>
2022-06-29 20:05:28 +0700

    'progate'

---
+++ Progate/study1/page7/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+    	<div>
+    	  <h1>Halo, Ninja Ken!</h1>
+    	  {/* Tambahkan tag <button> dengan teks "Guru Domba" */}
+    	  <button>Guru Domba</button>
+    	  <button>Ninja Ken</button>
+    	  
+    	  {/* Tambahkan tag <button> dengan teks "Ninja Ken" */}
+    	  
+    	  
+    	</div>
+    );
+  }
+}
+
+export default App;
